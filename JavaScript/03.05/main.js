487bf3a1d78379857c0533410be42baedc8d615e
Tural Guliyev <tguliev9@gmail.com>
2022-05-09 18:49:04 +0400

    new

--- JavaScript/03.05/main.js
+++ JavaScript/03.05/main.js
@@ -1,19 +1,42 @@
+class HtmlBlock {
+    constructor({styles = null, children = null}={}) {
+        this.styles = styles == null ? [] : styles
+        this.children = children == null ? [] : children
+    }
+
+    addElement(element) {
+        this.children.push(element)
+    }
+
+    addStyle(style) {
+        this.styles.push(style)
+    }
+
+    getCode() {
+        let style = document.createElement('style')
+        this.styles.forEach(styleClass => style.innerHTML += styleClass.getCSS())
+        let code = style.outerHTML
+        this.children.forEach(child => code += child.getHtml())
+        return code        
+    }
+}
+
 class HtmlElement {
-    constructor(name, isClosing, {id = null, innerHtml = null, attributes = null, styles = null, children = null}) {
+    constructor(name, isClosing, {innerHtml = null, attributes = null, styles = null, children = null}={}) {
         this.name = name
         this.isClosing = isClosing
-        this.id = id
         this.innerHtml = innerHtml
-        this.attributes = attributes
-        this.styles = styles
-        this.children = children
+        this.attributes = attributes == null ? new Map() : attributes
+        this.styles = styles == null ? new Map() : styles
+        this.children = children == null ? [] : children
     }
 
     addStyle(style, value) {
-        if (this.styles == null) this.styles = new Map()
-        if (this.styles == null) console.log('null')
-        this.styles[style] = value
-        // console.log(this.styles[style])
+        this.styles.set(style, value)
+    }
+
+    addAttribute(name, value) {
+        this.attributes.set(name, value)
     }
 
     append(element) {
@@ -26,21 +49,96 @@ class HtmlElement {
 
     getHtml() {
         let elem = document.createElement(this.name)
-        if (this.id != null) elem.id = this.id
         if (this.innerHtml != null) elem.innerHTML += this.innerHtml
-        if (this.attributes != null) this.attributes.forEach((value, key) => elem.setAttribute(key, value) );
-        if (this.styles != null) this.styles.forEach((value, key) => elem.style[key] = value )
-        if (this.children != null) this.children.forEach(child => elem.innerHTML += child.getHtml() )
+        this.attributes.forEach((value, key) => elem.setAttribute(key, value) );
+        this.styles.forEach((value, key) => elem.style[key] = value )
+        this.children.forEach(child => elem.innerHTML += child.getHtml() )
         return elem.outerHTML
     }
 }
 
-let h1 = new HtmlElement('h1', true, {innerHtml: 'Hello My Incredible Future', styles: new Map([['color', 'white'], ['backgroundColor', 'blue']])})
-let p = new HtmlElement('p', true, {innerHtml: 'Hello My Incredible Future', styles: new Map([['color', 'red'], ['backgroundColor', 'blue']])})
-let div1 = new HtmlElement('div', true, {styles: new Map([['backgroundColor', 'yellow']])})
-let div = new HtmlElement('div', true, {id: 'wrapper', attributes: new Map([['attribute1', 'value1'], ['attribute2', 'value2']]), children: [div1, p, h1]})
-div.addStyle('backgroundColor', 'white')
-div.addStyle('margin', '20px')
-div.addStyle('height', '100%')
-// div.styles.forEach((value, key) => console.log(key + value))
-document.body.innerHTML = div.getHtml()
+class CssClass {
+    constructor(className, styles = null) {
+        this.className = className
+        this.styles = styles == null ? new Map() : styles
+    }
+
+    addStyle(style, value) {
+        this.styles.set(style, value)
+    }
+
+    removeStyle(style) {
+        this.styles.delete(style)
+    }
+
+    getCSS() {
+        let css = `.${this.className} {`
+        this.styles.forEach((value, key) => css += `${key}: ${value};`)
+        css += '}'
+        return css
+    }
+}
+
+if (false) {
+    let wrapper = new HtmlElement('div', true)
+    wrapper.addAttribute('id', 'wrapper')
+    wrapper.addStyle('display', 'flex')
+
+    let div = new HtmlElement('div', true, {styles: new Map([['width', '300px'], ['margin', '10px']])})
+    let h3 = new HtmlElement('h3', true, {innerHtml: 'What is Lorem Ipsum?'})
+    let img = new HtmlElement('img', false, {styles: new Map([['width', '100%']]), attributes: new Map([['alt', 'Lorem Ipsum'], ['src', './lipsum.gif']])})
+    let p = new HtmlElement('p', true, {styles: new Map([['text-align', 'justify']])})
+    let a = new HtmlElement('a', true, {innerHtml: 'More...', attributes: new Map([['href', 'https://www.lipsum.com/'], ['target', '_blank']])})
+    p.innerHtml = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    p.append(a)
+    div.append(h3)
+    div.append(img)
+    div.append(p)
+    wrapper.append(div)
+    wrapper.append(div)
+
+    document.body.innerHTML = wrapper.getHtml()
+}
+
+if (true) {
+    let codeBlock = new HtmlBlock()
+
+    let wrapper = new HtmlElement('div', true)
+    wrapper.addAttribute('id', 'wrapper')
+    wrapper.addAttribute('class', 'wrap')
+    wrapper.addStyle('display', 'flex')
+
+    let div = new HtmlElement('div', true, {attributes: new Map([['class', 'block']])})
+    let h3 = new HtmlElement('h3', true, {innerHtml: 'What is Lorem Ipsum?'})
+    let img = new HtmlElement('img', false, {attributes: new Map([['class', 'img'], ['alt', 'Lorem Ipsum'], ['src', './lipsum.gif']])})
+    let p = new HtmlElement('p', true, {attributes: new Map([['class', 'text']])})
+    let a = new HtmlElement('a', true, {innerHtml: 'More...', attributes: new Map([['href', 'https://www.lipsum.com/'], ['target', '_blank']])})
+    p.innerHtml = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    p.append(a)
+    div.append(h3)
+    div.append(img)
+    div.append(p)
+    wrapper.append(div)
+    wrapper.append(div)
+
+    let wrapStyle = new CssClass('wrap')
+    wrapStyle.addStyle('display', 'flex')
+    
+    let blockStyle = new CssClass('block')
+    blockStyle.addStyle('width', '300px')
+    blockStyle.addStyle('margin', '10px')
+    
+    let imgStyle = new CssClass('img')
+    imgStyle.addStyle('width', '100%')
+
+    let textStyle = new CssClass('text')
+    textStyle.addStyle('text-align', 'justify')
+
+    codeBlock.addStyle(wrapStyle)
+    codeBlock.addStyle(blockStyle)
+    codeBlock.addStyle(imgStyle)
+    codeBlock.addStyle(textStyle)
+
+    codeBlock.addElement(wrapper)
+    document.body.innerHTML = codeBlock.getCode()
+}
