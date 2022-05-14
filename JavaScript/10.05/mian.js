288057802bcf9f4ef3efef6601d15fc7f152ed3f
Tural Guliyev <tguliev9@gmail.com>
2022-05-14 23:32:29 +0400

    new

--- JavaScript/10.05/mian.js
+++ JavaScript/10.05/mian.js
@@ -9,8 +9,8 @@ $(() => {
     bold.click(() => text.toggleClass('bold'))
     italic.click(() => text.toggleClass('italic'))
     underline.click(() => text.toggleClass('underline'))
-    fonts.change(() => text.css({fontFamily: $(this).val()}))
-    colors.change(() => text.css({color: $(this).val()}))
+    fonts.change(() => text.css({fontFamily: $(fonts).val()}))
+    colors.change(() => text.css({color: $(colors).val()}))
 
     fonts.val('monospace')
     colors.val('black')
